86f3abb262ca6a281829b287a66b19ef9b8f1fd9
MARIA ALVES <maria.alves270@etec.sp.gov.br>
2023-11-26 23:29:55 -0300

    TCC

---
+++ Controllers/Cadastro1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TelmaBolossite.Controllers
+{
+    public class Cadastro1Controller : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
